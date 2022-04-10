c5a2132e07d967357ce4899f97d134dfb72a6d91
Brett <brett.bedrin@kaplan.com>
2022-04-10 18:25:22 -0400

    ci: updated url

--- scripts/install-pandoc.sh
+++ scripts/install-pandoc.sh
@@ -1,5 +1,2 @@
-RELEASES_URL='https://github.com/jgm/pandoc/releases'
-export PANDOCVERSION=$(curl -I "$RELEASES_URL/latest" | sed -ne 's#Location:.*tag/\(.*\)$#\1#p' | tr -d "\n\r")
-echo $PANDOCVERSION
-wget $RELEASES_URL/download/$PANDOCVERSION/pandoc-$PANDOCVERSION-linux-amd64.tar.gz
-tar xvzf pandoc-$PANDOCVERSION-linux-amd64.tar.gz
+wget https://github.com/jgm/pandoc/releases/download/2.18/pandoc-2.18-linux-amd64.tar.gz
+tar xvzf pandoc-2.18-linux-amd64.tar.gz
